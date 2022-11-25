47a4b4d4aff1148ae931274a0f5fc47ef0bdd99d
Valentin LEPILLER <vlepille@e2r4p10.clusters.42paris.fr>
2022-11-25 18:46:35 +0100

    Repair every bonus header

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vlepille <vlepille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/25 17:52:41 by vlepille          #+#    #+#             */
-/*   Updated: 2022/11/25 17:52:42 by vlepille         ###   ########.fr       */
+/*   Updated: 2022/11/25 18:45:54 by vlepille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # include <stddef.h>
 
